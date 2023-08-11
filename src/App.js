d8f93741bccf7e8bc1cd2816fa9c9abbedad2342
ritikaj23 <ritikaj@skillup.online>
2023-08-11 03:49:51 -0400

    week3-convert static to dynamic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
-import { Navbar } from './Components/Nav_bar/Navbar';
+import { LandingPage } from './Components/Landing_Page/LandingPage';
+import Login from './Components/Login/Login';
+import Navbar from './Components/Nav_bar/Navbar';
+
+
+
+import SignUp from './Components/Sign_up/sign_up';
+
+
+
 
 
 function App() {
   return (
     <>
     <Navbar/>
+    <LandingPage/>
+    <Login/>
+    <SignUp/>
     </>
   );
 }
